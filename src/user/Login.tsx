a3ec55e01d4a61de316f0b4376ac69b9ea68af4a
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-12-29 20:54:51 +0530

    login UI done

--- src/user/Login.tsx
+++ src/user/Login.tsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
+import Container from "../Container";
+
 const Login = () => {
     return (
         <>
-            <h1>Login</h1>
-            <div className="border w-40">
-                <form>
-                    <div>
-                        <input placeholder="Email" className="border border-black-100" />
-                    </div>
-                    <div>
-                        <input placeholder="Password" className="border border-black"></input>
-                    </div>
-                    <button className="w-full bg-red-400">Login</button>
+            <div className="flex flex-col gap-10 border border-yellow-400 justify-center items-center py-20">
+                <div className="w-96 border border-green-400 flex flex-col gap-10 bg-red-200 py-10">
+                    <h1>Login</h1>
+                    <form className="border border-red-300 w-96 flex flex-col items-center">
+                        <div className="flex flex-col w-72 ">
+                            <input placeholder="Email" className="border border-black-100" />
+                            <input placeholder="Password" className="border border-black-100"></input>
+                        </div>
+                        <button className="w-2/3 bg-red-400 mt-10">Login</button>
+                        <p>Forgot password <Link to='/' className="text-blue-500">Register here</Link></p>
 
-                </form>
+                    </form>
+                </div>
             </div>
         </>
     );
