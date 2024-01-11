f9ba274c6f4d2e47f60bf00ba8e866b6676c30a4
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2024-01-11 20:13:53 +0530

    dropdown z-index done

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -37,7 +37,8 @@ const Navbar = () => {
 
     return (
         <Container>
-            <div className="Navbar h-20 border-b border-black-300 flex flex-row justify-between items-center">
+            {/* IF YOU WANT TO SHOW DROPDOWN ABOVE ALL OTHER COMPONENTS THEN GIVE Z INDEX TO NAV  */}
+            <div className="Navbar relative z-50 h-20 border-b border-black-300 flex flex-row justify-between items-center">
                 {/* <div className="border border-blue-400" style={{ display: "flex", justifyContent: "space-between" }}> */}
                 <Link to='/' className="logo">
                     <p className="font-sans font-bold"> <p className="text-sm">Mridul Bhai ka</p> Amazon</p>
